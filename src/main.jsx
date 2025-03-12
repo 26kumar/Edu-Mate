ae5d1e7f86b342cd6fc17c4abd7fa8dff5b23e05
26kumar <rupesh583k@gmail.com>
2025-03-13 04:00:24 +0530

    using  hash router instead of browser router

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,10 @@
-import { createRoot } from 'react-dom/client'
-import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { HashRouter } from "react-router-dom";
+import App from "./App";
 
-createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <HashRouter>
     <App />
-  </BrowserRouter>
-)
+  </HashRouter>
+);
